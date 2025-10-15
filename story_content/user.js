55283a070fda11a13911ae6619cd11bb3a2efcf1
MediaPembelajaranInteraktif <chandratripambudi5@gmail.com>
2025-10-15 21:09:28 +0700

    A

---
+++ story_content/user.js
@@ -0,0 +1,117 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6VkNlmzQQLN":
+        Script1();
+        break;
+      case "5vRpXZ4ea7r":
+        Script2();
+        break;
+      case "6QjyMtEEChH":
+        Script3();
+        break;
+      case "6FPVRyX0xMl":
+        Script4();
+        break;
+      case "6eSnrXRoiWq":
+        Script5();
+        break;
+      case "5d01m1LJli6":
+        Script6();
+        break;
+      case "5t47Dm0uXYZ":
+        Script7();
+        break;
+      case "6RQcfdbLqJ3":
+        Script8();
+        break;
+      case "5f8Y9f57smm":
+        Script9();
+        break;
+      case "6ZWSKGivi9j":
+        Script10();
+        break;
+      case "6dH2QQh8qUS":
+        Script11();
+        break;
+      case "6Rn2G6nPbz8":
+        Script12();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSong');
+audio.src = 'BackSound.mp3';
+audio.load();
+audio.play();
+}
+
+function Script2()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0;
+}
+
+function Script3()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.1;
+}
+
+function Script4()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.2;
+}
+
+function Script5()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.3;
+}
+
+function Script6()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.4;
+}
+
+function Script7()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.5;
+}
+
+function Script8()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.6;
+}
+
+function Script9()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.7;
+}
+
+function Script10()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.8;
+}
+
+function Script11()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.9;
+}
+
+function Script12()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 1;
+}
+
